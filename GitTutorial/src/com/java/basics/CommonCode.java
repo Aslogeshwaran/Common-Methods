b5ec06c71fc0b3cb60854ac7aca2745d7cdb351b
KumarTamilan <kumaranharsh123@gmail.com>
2024-11-11 16:34:55 +0530

    version updated

--- GitTutorial/src/com/java/basics/CommonCode.java
+++ GitTutorial/src/com/java/basics/CommonCode.java
@@ -1,13 +1,15 @@
 package com.java.basics;
 
-import org.openqa.selenium.WebDriver;
 
 public class CommonCode {
  
-	static WebDriver driver;
 	
 	public static void maximize()
 	{
+		System.out.println("Hi");
+	}
+	
+	public static void main(String[] args) {
 		
 	}
 }
